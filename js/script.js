bbc176dc1cd135e89a3b0ccabf7f5b7e2f769d31
Sandunjayasekar <jmsrjayasekara97@gmail.com>
2023-10-31 21:15:36 +0530

    navbar sticky js codes added

--- js/script.js
+++ js/script.js
@@ -3,14 +3,32 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 
+
+
 /*==================== scroll sections active link ====================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let heigt = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
+        if(top >= offset && top < offset + heigt) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+
+    });
 
     /*==================== sticky navbar ====================*/
+    
+    let header = document.querySelector('header');
+    header.classList('sticky', window.scrollY > 100);
 
     /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
 };
